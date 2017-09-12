1d2db3e7315fc489de4ecfe4fb07b81dd6c9703a
Andika <akidnaa@gmail.com>
2017-09-12 18:51:39 +0700

    update database

--- ci_shop.sql
+++ ci_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: 09 Sep 2017 pada 21.31
+-- Generation Time: 12 Sep 2017 pada 18.50
 -- Versi Server: 5.6.36
 -- PHP Version: 5.6.31
 
@@ -41,13 +41,11 @@ CREATE TABLE `brands` (
 --
 
 INSERT INTO `brands` (`brand_id`, `brand_name`, `brand_logo`, `brand_description`, `brand_slug`) VALUES
-(1, 'Adidas', 'logo.jpg', 'deskripsi adidas', 'adidas'),
-(2, 'Cardinal', 'logo.jpg', 'deskripsi cardinal', 'cardinal'),
-(3, 'Eiger', 'logo.jpg', 'deskripsi eiger', 'eiger'),
-(8, 'Aku', '', 'ini aku lho\r\n', 'aku'),
-(9, 'Nganu', '', 'ini nganu', 'nganu'),
-(10, 'Merk A', '', 'ini Merk A', 'merk-a'),
-(11, 'New Era', '', 'Produk yang sangat berkualitas', 'new-era');
+(1, 'Adidas', 'default.jpg', 'deskripsi adidas', 'adidas'),
+(2, 'Cardinal', 'default.jpg', 'deskripsi cardinal', 'cardinal'),
+(3, 'Eiger', 'default.jpg', 'deskripsi eiger', 'eiger'),
+(11, 'New Era', 'default.jpg', 'Produk yang sangat berkualitas', 'new-era'),
+(13, 'Brand Asik', 'default.jpg', 'Deskripsi brand asik', 'brand-asik');
 
 -- --------------------------------------------------------
 
@@ -74,6 +72,31 @@ INSERT INTO `categories` (`category_id`, `category_name`, `category_description`
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `preferences`
+--
+
+CREATE TABLE `preferences` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `preferences`
+--
+
+INSERT INTO `preferences` (`id`, `type`, `name`, `value`) VALUES
+(1, 'site', 'site_name', 'CiShop'),
+(2, 'site', 'site_description', 'Online Shop dengan Codeigniter'),
+(3, 'email', 'smtp_host', 'ssl://smtp.googlemail.com'),
+(4, 'email', 'smtp_port', '465'),
+(5, 'email', 'smtp_user', 'example@gmail.com'),
+(6, 'email', 'smtp_pass', 'password');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `products`
 --
 
@@ -87,6 +110,40 @@ CREATE TABLE `products` (
   `product_selling_price` double NOT NULL,
   `product_slug` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `product_photos`
+--
+
+CREATE TABLE `product_photos` (
+  `photo_id` int(10) UNSIGNED NOT NULL,
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `photo_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_alias` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_photo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_alias`, `user_email`, `user_password`, `user_photo`) VALUES
+(1, 'user', 'John Doe', 'example@example.com', '12dea96fec20593566ab75692c9949596833adc9', 'user.jpg');
 
 --
 -- Indexes for dumped tables
@@ -108,6 +165,13 @@ ALTER TABLE `categories`
   ADD UNIQUE KEY `unique_cateogry_id` (`category_id`);
 
 --
+-- Indexes for table `preferences`
+--
+ALTER TABLE `preferences`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_id` (`id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
@@ -118,6 +182,22 @@ ALTER TABLE `products`
   ADD KEY `index_brand_id` (`brand_id`);
 
 --
+-- Indexes for table `product_photos`
+--
+ALTER TABLE `product_photos`
+  ADD PRIMARY KEY (`photo_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `unique_user_id` (`user_id`),
+  ADD UNIQUE KEY `unique_user_name` (`user_name`),
+  ADD UNIQUE KEY `unique_user_email` (`user_email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -125,17 +205,32 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `brand_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `brand_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `preferences`
+--
+ALTER TABLE `preferences`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `product_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_photos`
+--
+ALTER TABLE `product_photos`
+  MODIFY `photo_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
@@ -146,6 +241,12 @@ ALTER TABLE `products`
 ALTER TABLE `products`
   ADD CONSTRAINT `lnk_brands_products` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`brand_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `lnk_categories_products` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `product_photos`
+--
+ALTER TABLE `product_photos`
+  ADD CONSTRAINT `product_photos_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
