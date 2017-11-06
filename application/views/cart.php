c4cbd648c1a0b18640fd8cade069b4368b31fd67
Andika <akidnaa@gmail.com>
2017-11-06 21:54:23 +0700

    ongkir

--- application/views/cart.php
+++ application/views/cart.php
@@ -38,6 +38,36 @@
 		</div>
 		<div class="produced">
 			<?php if (!empty($this->session->userdata('login'))) : ?>
+				<form action="<?= site_url('cart/save_address') ?>" method="post">
+					<?php 
+					$user = $this->session->userdata('login'); ?>
+					<div class="col-md-6">
+						<div class="form-group">
+							<input type="text" name="address_to" class="form-control" value="<?= $user['user_alias'] ?>" placeholder="Nama Penerima">
+						</div>
+						<div class="form-group">
+							<input type="text" name="address_phone" class="form-control" placeholder="No Handphone Penerima">
+						</div>
+						<div class="form-group">
+							<select name="address_city" class="form-control" placeholder="Kota Tujuan">
+								<option value=""></option>
+							</select>
+						</div>
+						<div class="form-group">
+							<select name="service" class="form-control" placeholder="Layanan">
+								<option value=""></option>
+							</select>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group">
+							<input type="text" name="ongkir" class="form-control" readonly="readonly">
+						</div>
+						<div class="form-group">
+							<input type="text" name="total" class="form-control" readonly="readonly" value="<?= number_format($this->cart->total(),2,',','.'); ?>">
+						</div>
+					</div>
+				</form>
 				<a href="<?= site_url('cart/checkout') ?>" class="hvr-skew-backward">Produced To Buy</a>
 			<?php else : ?>
 				<a href="<?= site_url('auth/login') ?>" class="hvr-skew-backward">Login To Process</a>
@@ -56,3 +86,53 @@
 <!--//brand-->
 <!--//content-->
 <?php $this->load->view('layout/footer'); ?>
+<script>
+	$(document).ready(function() {
+		// request data city / kota ke rajaongkir
+		$.ajax({
+			url: 'https://api.rajaongkir.com/starter/city',
+			method: 'get',
+			data: {
+				key: 'be50f8a185202f670a47e1a6967dce7a'
+			}
+		}).success(function(data) {
+			var options = '';
+			// dari setiap city yg didapat
+			data.rajaongkir.results.forEach(function(item) {
+				// bikin option dari data city yg didapat
+				options += '<option value="'+item.city_id+'">'+item.city_name+'</option>'
+			})
+			// masukkan options yg sudah dibuat ke dalam select
+			$('select[name="address_city"]').html(options)
+		})
+
+		// ketika memilih city
+		$('select[name="address_city"]').change(function() {
+			var city = $(this).val()
+			$.ajax({
+				url: 'https://api.rajaongkir.com/starter/cost',
+				method: 'post',
+				data: {
+					key: 'be50f8a185202f670a47e1a6967dce7a',
+					origin: 501,
+					destination: city,
+					weight: 1000,
+					courier: 'jne'
+				}
+			}).success(function(data) {
+				var options = '';
+				// dari setiap city yg didapat
+				data.rajaongkir.results[0].costs.forEach(function(item) {
+					// bikin option dari data city yg didapat
+					options += '<option value="'+item.cost[0].value+'">'+item.service+'</option>'
+				})
+				$('select[name="service"]').html(options)
+			})
+			// jika pilih service oke / reg / yes
+			$('select[name="service"]').change(function() {
+				$('input[name="ongkir"]').val($(this).val())
+				$('input[name="total"]').val(parseInt($(this).val())+parseInt(<?= $this->cart->total() ?>))
+			})
+		})
+	})
+</script>
