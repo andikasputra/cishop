414e3286d6657c62028f7a192d560fb69f42ce1f
Andika <akidnaa@gmail.com>
2018-02-06 22:00:12 +0700

    style pagination

--- application/controllers/admin/master/Products.php
+++ application/controllers/admin/master/Products.php
@@ -21,6 +21,22 @@ class Products extends CI_Controller {
 		$config['per_page'] = 5;
 		$config['uri_segment'] = 5;
 		$config['total_rows'] = $this->m_product->get_total_data();
+		// style pagination
+		// wrap entire pagination
+		$config['full_tag_open'] = '<nav aria-label="Page navigation"><ul class="pagination">';
+		$config['full_tag_close'] = '</ul></nav>';
+		// number page
+		$config['num_tag_open'] = '<li>';
+		$config['num_tag_close'] = '</li>';
+		// next page
+		$config['next_tag_open'] = '<li>';
+		$config['next_tag_close'] = '</li>';
+		// previous page
+		$config['prev_tag_open'] = '<li>';
+		$config['prev_tag_close'] = '</li>';
+		// current page
+		$config['cur_tag_open'] = '<li class="active"><a href="#">';
+		$config['cur_tag_close'] = '</a></li>';
 		$start = $this->uri->segment(5);
 		// echo $start;
 		// print_r($this->m_product->get_total_data());exit();
